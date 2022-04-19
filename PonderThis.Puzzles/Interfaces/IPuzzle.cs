a4c560e2eff171c5be64b397ab0eb527ee8d49a1
Zackary Lowery <zlowery@xcjs.com>
2022-04-18 22:41:41 -0400

    Define IPuzzle.

---
+++ PonderThis.Puzzles/Interfaces/IPuzzle.cs
@@ -0,0 +1,11 @@
+﻿namespace PonderThis.Puzzles.Interfaces;
+
+public interface IPuzzle
+{
+    DateTime PuzzleDate { get; }
+    string Description { get; }
+    string? CalculatedAnswer { get; }
+    string? CorrectAnswer { get; }
+
+    void Solve();
+}
