e56bd136784ac7ab7bf88c426e4ff0b68239b784
Zackary Lowery <zlowery@xcjs.com>
2022-04-18 23:23:47 -0400

    Add CLI and test client for Ponder This puzzles.

--- PonderThis.Tests/PuzzleTests.cs
+++ PonderThis.Tests/PuzzleTests.cs
@@ -1,4 +1,8 @@
-﻿using Xunit;
+﻿using PonderThis.Puzzles.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 
 namespace PonderThis.Tests
 {
@@ -7,7 +11,34 @@ namespace PonderThis.Tests
         [Fact]
         public void TestAll()
         {
+            var puzzleBase = typeof(IPuzzle);
+            var puzzleTypes = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(type => puzzleBase.IsAssignableFrom(type) && !type.IsInterface)
+                .ToList();
 
+            var puzzles = new List<IPuzzle>();
+
+            foreach (var puzzleType in puzzleTypes)
+            {
+                IPuzzle? puzzle = Activator.CreateInstance(puzzleType) as IPuzzle;
+
+                if (puzzle != null)
+                {
+                    puzzles.Add(puzzle);
+                }
+            }
+
+            puzzles = puzzles.OrderBy(p => p.PuzzleDate).ToList();
+
+            foreach (var puzzle in puzzles)
+            {
+                Console.WriteLine($"Solving \"Ponder This\" released on {puzzle.PuzzleDate.ToString("yyyy-MM")}{Environment.NewLine}");
+                Console.WriteLine($"{puzzle.Description}{Environment.NewLine}");
+                puzzle.Solve();
+                Assert.Equal(puzzle.CorrectAnswer, puzzle.ComputedAnswer);
+                Console.WriteLine(Environment.NewLine);
+            }
         }
     }
 }
--- PonderThis/Program.cs
+++ PonderThis/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+
+using PonderThis.Puzzles.Interfaces;
+
+namespace PonderThis;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var puzzleBase = typeof(IPuzzle);
+        var puzzleTypes = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(x => x.GetTypes())
+            .Where(type => puzzleBase.IsAssignableFrom(type) && !type.IsInterface)
+            .ToList();
+
+        var puzzles = new List<IPuzzle>();
+
+        foreach(var puzzleType in puzzleTypes)
+        {
+            IPuzzle? puzzle = Activator.CreateInstance(puzzleType) as IPuzzle;
+
+            if(puzzle != null)
+            {
+                puzzles.Add(puzzle);
+            }            
+        }
+
+        puzzles = puzzles.OrderBy(p => p.PuzzleDate).ToList();
+
+        foreach (var puzzle in puzzles)
+        {
+            Console.WriteLine($"Solving \"Ponder This\" released on {puzzle.PuzzleDate.ToString("yyyy-MM")}{Environment.NewLine}");
+            Console.WriteLine($"{puzzle.Description}{Environment.NewLine}");
+            puzzle.Solve();
+            Console.WriteLine($"Computed answer: {puzzle.ComputedAnswer}");
+            Console.WriteLine($"Correct answer:  {puzzle.CorrectAnswer}{Environment.NewLine}");
+        }
+    }
+}
